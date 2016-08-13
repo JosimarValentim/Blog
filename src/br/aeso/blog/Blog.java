890b5d4126adb24b91dddf934fce4b61282449a5
Josimar Valentim <mazinhojb2@hotmail.com>
2016-08-13 13:40:01 -0300

    Add conteudo alteração do blog

--- src/br/aeso/blog/Blog.java
+++ src/br/aeso/blog/Blog.java
@@ -1,5 +1,11 @@
 package br.aeso.blog;
 
+import java.util.List;
+
 public class Blog {
+	private String data;
+	private Usuario dono;
+	private String titulo;
+	private List<Conteudo> ListaConteudo;
 
 }
---
+++ src/br/aeso/blog/Conteudo.java
@@ -0,0 +1,9 @@
+package br.aeso.blog;
+
+public class Conteudo {
+	
+	private String data;
+	private String texto;
+	private Usuario autor;
+
+}
