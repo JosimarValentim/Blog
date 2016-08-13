7bf644304c4354049803bda880f458984d93591a
Josimar Valentim <mazinhojb2@hotmail.com>
2016-08-13 13:55:40 -0300

    class blog editada

--- src/br/aeso/blog/Blog.java
+++ src/br/aeso/blog/Blog.java
@@ -7,5 +7,43 @@ public class Blog {
 	private Usuario dono;
 	private String titulo;
 	private List<Conteudo> ListaConteudo;
+	
+	public Blog(String data, Usuario dono, String titulo, List<Conteudo> ListaConteudo){
+		this.data = data;
+		this.dono = dono;
+		this.titulo = titulo;
+		this.ListaConteudo = ListaConteudo;
+	}
+	
+	public String getData(){
+		return data;
+	}
+	public void setData(String data){
+		this.data = data;
+	}
+	public Usuario getDono(){
+		return dono;
+	}
+	public void setDono(Usuario dono){
+		this.dono = dono;
+	}
+	public String getTitulo(){
+		return titulo;
+	}
+	public void setTitulo(String titulo){
+		this.titulo = titulo;
+	}
+
+	public List<Conteudo> getListaConteudo() {
+		return ListaConteudo;
+	}
+	public void setListaConteudo(List<Conteudo> listaConteudo) {
+		ListaConteudo = listaConteudo;
+	}
+	public String toString() {
+		return "Blog [data=" + data + ", dono=" + dono + ", titulo=" + titulo + ", ListaConteudo=" + ListaConteudo
+				+ "]";
+	}
+	
 
 }
